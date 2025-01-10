72dc510497a2e1b9e47624c267d01eb3676c2596
K-Fahrenberger <fahrenberger.k@web.de>
2025-01-10 13:44:51 +0100

    fix:resize works as intended

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-const SNOWFLAKE_NUMBER = 200;
-const SNOWFLAKE_SIZE_INITIAL = 3;
+const SNOWFLAKE_NUMBER = 420;
+const SNOWFLAKE_SIZE_INITIAL = 8;
 const SNOWFLAKE_SPEED_INITIAL = 2;
 const SNOWFLAKE_COLOR = "white";
 
 const canvas = document.getElementById("myCanvas");
 const context = canvas.getContext("2d");
 const snowflakes = [];
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+resizeCanvas();
+
 const createSnowflake = () => ({
   x: Math.random() * canvas.width,
   y: Math.random() * canvas.height,
   radius: Math.floor(Math.random() * SNOWFLAKE_SIZE_INITIAL) + 2,
   color: SNOWFLAKE_COLOR,
   speed: Math.random() * SNOWFLAKE_SPEED_INITIAL + 1,
-  sway: Math.random() - 0.5,
 });
 
 function drawSnowflake(snowflake) {
@@ -25,7 +32,6 @@ function drawSnowflake(snowflake) {
 
 function updateSnowflake(snowflake) {
   snowflake.y += snowflake.speed;
-  snowflake.x += snowflake.sway;
   if (snowflake.y > canvas.height) {
     snowflake.y = 0;
     snowflake.x = Math.random() * canvas.width;
@@ -48,9 +54,8 @@ function animateSnowflakes() {
 
   requestAnimationFrame(animateSnowflakes);
 }
-window.addEventListener("resize", () => {
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerHeight;
-});
+
+window.addEventListener("resize", resizeCanvas);
+
 initSnowflakes();
 animateSnowflakes();
